cd74a257a5243ea531557bf561991288f9deda8d
Timur Kristóf <Timur@Timur-Amilo.(none)>
2011-08-06 15:22:53 +0200

    ChannelModel now has new properties and a method to fake messages. (testability)

--- model/channelmodel.cpp
+++ model/channelmodel.cpp
@@ -4,6 +4,7 @@ ChannelModel::ChannelModel(QString name, QObject *parent) :
     QObject(parent)
 {
     _name = name;
+    fakeMessage();
 }
 
 void ChannelModel::close()
@@ -11,7 +12,23 @@ void ChannelModel::close()
     // TODO
 }
 
-void ChannelModel::sendMessage(const QString &message)
+void ChannelModel::sendCurrentMessage()
 {
-    // TODO
+    // TODO: send message
+    qDebug() << currentMessage() << "was sent to" << name();
+    setCurrentMessage(QString());
+}
+
+void ChannelModel::fakeMessage()
+{
+    //qDebug() << "faking new message";
+    //UserModel *u = new UserModel();
+    //u->setName("Zvdegor");
+    MessageModel *m = new MessageModel();
+    //m->setUser(u);
+    m->setText(QUuid::createUuid().toString());
+    m->setTimestamp(QTime::currentTime().toString("HH:mm"));
+    _messages.addItem(m);
+    //if (_messages.rowCount() < 15)
+        QTimer::singleShot(1000, this, SLOT(fakeMessage()));
 }
--- model/channelmodel.h
+++ model/channelmodel.h
@@ -12,23 +12,27 @@ class ChannelModel : public QObject
     Q_OBJECT
     GENPROPERTY(QString, _name, name, setName, nameChanged)
     Q_PROPERTY(QString name READ name WRITE setName NOTIFY nameChanged)
-    GENPROPERTY_PTR(QObjectListModel<UserModel>*, _users, users, setUsers, usersChanged)
-    Q_PROPERTY(QObjectListModel<UserModel>* users READ users WRITE setUsers NOTIFY usersChanged)
-    GENPROPERTY_PTR(QObjectListModel<MessageModel>*, _messages, messages, setMessages, messagesChanged)
-    Q_PROPERTY(QObjectListModel<MessageModel>* messages READ messages WRITE setMessages NOTIFY messagesChanged)
+    GENPROPERTY(QString, _currentMessage, currentMessage, setCurrentMessage, currentMessageChanged)
+    Q_PROPERTY(QString currentMessage READ currentMessage WRITE setCurrentMessage NOTIFY currentMessageChanged)
+    QObjectListModel<MessageModel> _messages;
 
 public:
     explicit ChannelModel(QString name = QString(), QObject *parent = 0);
+    Q_INVOKABLE QObject *messages() { return &_messages; }
 
 signals:
     void nameChanged();
     void usersChanged();
     void messagesChanged();
-    void messageReceived(const QString &message);
+    void currentMessageChanged();
+    void messageReceived(const MessageModel &message);
+
+private slots:
+    void fakeMessage();
 
 public slots:
     void close();
-    void sendMessage(const QString &message);
+    void sendCurrentMessage();
 
 };
 
