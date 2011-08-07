4acbd37ed759586a9b12e84bc2d144e57580acfa
Timur Kristóf <Timur@Timur-Amilo.(none)>
2011-08-07 03:14:07 +0200

    ChannelModel can now autocomplete nicknames.

--- model/channelmodel.cpp
+++ model/channelmodel.cpp
@@ -9,16 +9,14 @@ ChannelModel::ChannelModel(QString name, QObject *parent) :
 
 }
 
-void ChannelModel::close()
-{
-    // TODO
-}
-
 void ChannelModel::sendCurrentMessage()
 {
     // TODO: send message
-    qDebug() << currentMessage() << "was sent to" << name();
-    setCurrentMessage(QString());
+    if (_currentMessage.length() > 0)
+    {
+        qDebug() << currentMessage() << "was sent to" << name();
+        setCurrentMessage(QString());
+    }
 }
 
 void ChannelModel::fakeMessage()
@@ -31,5 +29,78 @@ void ChannelModel::fakeMessage()
     m->setTimestamp(QTime::currentTime().toString("HH:mm"));
     _messages.addItem(m);
     //if (_messages.rowCount() < 15)
-        QTimer::singleShot(1000, this, SLOT(fakeMessage()));
+    QTimer::singleShot(1000, this, SLOT(fakeMessage()));
+}
+
+void ChannelModel::setCurrentMessage(const QString &value)
+{
+    if (_completionFragment.length())
+        _completionFragment.clear();
+
+    _currentMessage = value;
+    emit currentMessageChanged();
+}
+
+void ChannelModel::autoCompleteNick()
+{
+    if (_currentMessage.length() == 0)
+    {
+        // The user hasn't typed anything, let's give him the name of the last spoke user
+
+        if (_messages.rowCount() > 0)
+            setCurrentMessage(_messages.lastItem()->userName());
+
+        return;
+    }
+    else if (_currentMessage[_currentMessage.length() - 1] == ' ')
+        return;
+
+    QString replacableFragment, newFragment;
+
+    if (_completionFragment.length())
+    {
+        // The user already started cycling through the list of possible nicks, give the next one
+
+        if (_possibleNickNames.count() <= 1)
+            return;
+
+        replacableFragment = _possibleNickNames[_currentCompletionIndex];
+        _currentCompletionIndex ++;
+
+        if (_currentCompletionIndex >= _possibleNickNames.count())
+            _currentCompletionIndex = 0;
+    }
+    else
+    {
+        // The user already has a message and is trying to start autocompleting a name
+
+        if (_currentMessage.contains(' '))
+        {
+            _currentCompletionPosition = _currentMessage.lastIndexOf(' ') + 1;
+            replacableFragment = _currentMessage.mid(_currentCompletionPosition);
+        }
+        else
+        {
+            _currentCompletionPosition = 0;
+            replacableFragment = _currentMessage;
+        }
+
+        _possibleNickNames.clear();
+        _currentCompletionIndex = 0;
+
+        foreach (QString nick, _users.stringList())
+            if (nick.startsWith(replacableFragment, Qt::CaseInsensitive))
+                _possibleNickNames.append(nick);
+
+        if (!_possibleNickNames.count())
+            return;
+
+        _completionFragment = replacableFragment;
+    }
+
+    newFragment = _possibleNickNames[_currentCompletionIndex];
+    //qDebug() << "replacable fragment is: " << replacableFragment << ", new fragment is: " << newFragment;
+
+    _currentMessage.replace(_currentCompletionPosition, replacableFragment.length(), newFragment);
+    emit currentMessageChanged();
 }
--- model/channelmodel.h
+++ model/channelmodel.h
@@ -13,13 +13,18 @@ class ChannelModel : public QObject
 
     GENPROPERTY(QString, _name, name, setName, nameChanged)
     Q_PROPERTY(QString name READ name WRITE setName NOTIFY nameChanged)
-    GENPROPERTY(QString, _currentMessage, currentMessage, setCurrentMessage, currentMessageChanged)
+    GENPROPERTY_R(QString, _currentMessage, currentMessage)
     Q_PROPERTY(QString currentMessage READ currentMessage WRITE setCurrentMessage NOTIFY currentMessageChanged)
-    Q_PROPERTY(QObject* users READ users)
-    Q_PROPERTY(int userCount READ userCount)
+    Q_PROPERTY(QObject* users READ users NOTIFY usersChanged)
+    Q_PROPERTY(int userCount READ userCount NOTIFY usersChanged)
 
     QObjectListModel<MessageModel> _messages;
     QStringListModel _users;
+
+    QString _completionFragment;
+    QList<QString> _possibleNickNames;
+    int _currentCompletionIndex, _currentCompletionPosition;
+
     friend class IrcModel;
 
 public:
@@ -27,16 +32,18 @@ public:
     Q_INVOKABLE QObject *messages() { return &_messages; }
     Q_INVOKABLE QStringListModel *users() { return &_users; }
     Q_INVOKABLE int userCount() { return _users.rowCount(); }
+    Q_INVOKABLE void autoCompleteNick();
+    void setCurrentMessage(const QString &value);
 
 signals:
     void nameChanged();
     void currentMessageChanged();
+    void usersChanged();
 
 private slots:
     void fakeMessage();
 
 public slots:
-    void close();
     void sendCurrentMessage();
 
 };
