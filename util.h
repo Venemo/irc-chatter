4cbce326c796af864fd30983f257dac525bd254f
Timur Kristóf <venemo@msn.com>
2011-08-14 16:21:05 +0200

    Property accessors are now const methods

--- util.h
+++ util.h
@@ -3,12 +3,12 @@
 
 #define GENPROPERTY(type, pname, name, settername, signalname) \
     private: type pname; \
-    public: type name() { return pname; } void settername (const type &value) { pname = value; emit signalname (); } \
+    public: type name() const { return pname; } void settername (const type &value) { pname = value; emit signalname (); } \
     private:
 
 #define GENPROPERTY_R(type, pname, name) \
     private: type pname; \
-    public: type name() { return pname; } \
+    public: type name() const { return pname; } \
     private:
 
 #define GENPROPERTY_PTR(type, pname, name, settername, signalname) \
